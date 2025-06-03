19ee5fd19264b7079f2f6bcc623c3393450c3d57
Jacob Butler <jjb23u@gmail.com>
2025-06-02 21:58:19 -0500

    modifying player class

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "1"
-import pygame
+import pygame # type: ignore
 import constants
+import player
 
 def main():
     pygame.init()
@@ -9,13 +10,21 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    ply = player.Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         black = [0,0,0]
         screen.fill(black)
+        ply.update(dt)
+        ply.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)
 
 if __name__ == "__main__":
     main()
